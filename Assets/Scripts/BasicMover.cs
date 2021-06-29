f240eb0e8398428407f07e0c3a5e7764b178bdb5
subbireddy143 <ksubbireddy2001@gmail.com>
2021-06-29 10:40:35 +0530

    Copying Project Files
    Uploading Initial project files

---
+++ Assets/Scripts/BasicMover.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMover : MonoBehaviour
+{
+    public float spin_Speed = 180.0f;
+    public float motion_Speed = 0.1f;
+
+    public bool doSpin = true;
+    public bool doMotion = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (doSpin)
+        {
+            //spin the object
+            gameObject.transform.Rotate(Vector3.up * spin_Speed * Time.deltaTime);
+        }
+
+        if (doMotion)
+        {
+            //move up and down
+            gameObject.transform.Translate(Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad) * motion_Speed);
+        }
+
+    }
+}
